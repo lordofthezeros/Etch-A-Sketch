5dbc21deefa2e87787bc3995c61201968bfc2e99
lordofthezeros <cee.pritchard@gmail.com>
2021-11-19 16:04:48 -0500

    Add functionality to dynamically reset and resize the grid

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
 const gridContainer = document.querySelector('.grid-container');
 const resetButton = document.querySelector('.reset-grid');
+const resizeButton = document.querySelector('.resize-grid');
+const slider = document.getElementById('grid-dims');
+let output = document.getElementById('grid-dim-value');
 
-const gridSize = 16;
+
+
+output.textContent = `${slider.value} x ${slider.value}`;
+
+slider.oninput = function () {
+    output.textContent = `${slider.value} x ${slider.value}`;
+}
+
+const DEFAULTGRIDSIZE = 16;
 
 function changeColour(e) {
     e.target.style.cssText = 'background-color: blue';
@@ -13,19 +24,30 @@ function clear() {
 
 function resetScreen() {
     clear();
-    setGrid();
+    slider.value = slider.defaultValue;
+    output.textContent = `${slider.value} x ${slider.value}`;
+    setGrid(DEFAULTGRIDSIZE);
+}
+
+function resizeScreen() {
+    clear();
+    let gridSize = parseInt(slider.value);
+    setGrid(gridSize);
 }
 
 // set initial grid
-function setGrid() {
-for (let i = 0; i < gridSize ** 2; i++) {
-    const cell = document.createElement('div');
-    cell.classList.add('grid-item');
-    cell.addEventListener('mouseover', changeColour)
-    gridContainer.appendChild(cell);
-}
+function setGrid(currentSize) {
+    gridContainer.style.gridTemplateColumns = `repeat(${currentSize}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${currentSize}, 1fr)`;
+    for (let i = 0; i < currentSize ** 2; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-item');
+        cell.addEventListener('mouseover', changeColour)
+        gridContainer.appendChild(cell);
+    }
 }
 
 resetButton.onclick = () => resetScreen();
+resizeButton.onclick = () => resizeScreen();
 
-window.onload = () => setGrid();
+window.onload = () => setGrid(DEFAULTGRIDSIZE);
